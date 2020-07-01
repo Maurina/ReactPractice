ae44642a453cc4a50fc87f6593ce2c8a39c89fb1
Maurina_Davis <maurina.davis@gmail.com>
2020-07-01 11:33:30 -0600

    state example, first simple component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Sara', age: 23},
+      {name: 'Anna', age: 24},
+      {name: 'Laura', age: 31},
+    ],
+    otherState: 'other value'
+  }
+
+  switchNameHandler = () => {
+    //DON'T DO IT LIKE THIS: this.state.persons[0].name = 'Jill';
+    this.setState({
+      persons: [
+        {name: 'Milley', age: 23},
+        {name: 'Anna', age: 24},
+        {name: 'Laura', age: 30},
+      ],
+    } )
+    //console.log('Was clicked!');
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,9 +32,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">First React App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobby is sewing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
